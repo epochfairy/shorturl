7974ded17a3c5f95aa736d78be6b8c9de905d0f5
MetFairy <66505223+epochfairy@users.noreply.github.com>
2020-08-10 11:23:10 +0800

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 
     public static void main(String[] args) {
+System.setProperty("spring.config.additional-location", "file:${user.home}/.halo/,file:${user.home}/halo-dev/");
         SpringApplication.run(DemoApplication.class, args);
     }
 
